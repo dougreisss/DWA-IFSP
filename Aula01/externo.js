dd970eba881cab2bc8df39df439928e16438f938
dougreisss <douglas_rrs@hotmail.com>
2024-09-15 14:01:34 -0300

    Initial commit

---
+++ Aula01/externo.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    alert('Método Externo');
+
+    console.log('apenas para programadores');
+
+    var copinho = confirm('Eu consegui fazer o TP');
+
+    document.write(copinho);
+
+};
